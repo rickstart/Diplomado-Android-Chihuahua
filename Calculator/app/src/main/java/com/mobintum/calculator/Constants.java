cda723490d25a197755bcdca15c7c65fa485d767
rickstart <ricardo.celj@gmail.com>
2015-05-17 15:14:42 -0500

    works sum less div mult

---
+++ Calculator/app/src/main/java/com/mobintum/calculator/Constants.java
@@ -0,0 +1,13 @@
+package com.mobintum.calculator;
+
+/**
+ * Created by Rick on 17/05/15.
+ */
+public class Constants {
+
+    public static final int PLUS = 1;
+    public static final int MULTIPLY = 2;
+    public static final int LESS = 3;
+    public static final int DIVISION = 4;
+    public static final int PERCENT = 5;;
+}
--- Calculator/app/src/main/java/com/mobintum/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/mobintum/calculator/MainActivity.java
@@ -1,17 +1,73 @@
 package com.mobintum.calculator;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
+    private TextView txtResult;
+    private Button btnAC, btnSign, btnPercent, btnDivision, btnMultiply, btnLess, btnPlus, btnEqual,
+    btnDot, btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine;
+
+    private double oper1 = 0.0;
+    private double oper2 = 0.0;
+
+    private boolean flag = true;
+    private int operation=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        btnAC = (Button) findViewById(R.id.btnAC);
+        btnSign = (Button) findViewById(R.id.btnSign);
+        btnPercent = (Button) findViewById(R.id.btnPercent);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnLess = (Button) findViewById(R.id.btnLess);
+        btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnEqual = (Button) findViewById(R.id.btnEqual);
+        btnDot = (Button) findViewById(R.id.btnDot);
+        btnZero = (Button) findViewById(R.id.btnZero);
+        btnOne = (Button) findViewById(R.id.btnOne);
+        btnTwo = (Button) findViewById(R.id.btnTwo);
+        btnThree = (Button) findViewById(R.id.btnThree);
+        btnFour = (Button) findViewById(R.id.btnFour);
+        btnFive = (Button) findViewById(R.id.btnFive);
+        btnSix = (Button) findViewById(R.id.btnSix);
+        btnSeven = (Button) findViewById(R.id.btnSeven);
+        btnEight = (Button) findViewById(R.id.btnEight);
+        btnNine = (Button) findViewById(R.id.btnNine);
+
+        btnAC.setOnClickListener(this);
+        btnSign.setOnClickListener(this);
+        btnPercent.setOnClickListener(this);
+        btnDivision.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btnLess.setOnClickListener(this);
+        btnPlus.setOnClickListener(this);
+        btnEqual.setOnClickListener(this);
+        btnDot.setOnClickListener(this);
+        btnZero.setOnClickListener(this);
+        btnOne.setOnClickListener(this);
+        btnTwo.setOnClickListener(this);
+        btnThree.setOnClickListener(this);
+        btnFour.setOnClickListener(this);
+        btnFive.setOnClickListener(this);
+        btnSix.setOnClickListener(this);
+        btnSeven.setOnClickListener(this);
+        btnEight.setOnClickListener(this);
+        btnNine.setOnClickListener(this);
+
+
+
     }
 
 
@@ -35,5 +91,158 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+
+            case R.id.btnDot:
+
+                String number = txtResult.getText().toString();
+                if(!number.contains(".")){
+                    txtResult.append(".");
+                    flag = true;
+
+                }
+                break;
+
+            case R.id.btnEqual:
+
+                if(oper1 != 0.0 && flag){
+
+                    oper2 = Double.valueOf(txtResult.getText().toString());
+                    double result = operation(operation,oper1,oper2);
+                    String sresult = Double.toString(result);
+                    sresult = sresult.replace(".0","");
+                    txtResult.setText(sresult);
+                    oper2 = 0.0;
+                    oper1 = result;
+                    flag = false;
+
+                }
+
+                break;
+
+            case R.id.btnAC:
+                txtResult.setText("");
+                flag = true;
+                oper1 = 0.0;
+                oper2 = 0.0;
+                break;
+
+            case R.id.btnMultiply:
+
+                if(oper1==0.0){
+                    oper1 = Double.valueOf(txtResult.getText().toString());
+                    flag = false;
+
+                }else if(oper2==0.0 && flag){
+                    oper2 = Double.valueOf(txtResult.getText().toString());
+
+                    double result = operation(operation, oper1, oper2);
+                    txtResult.setText(Double.toString(result));
+                    oper2 = 0.0;
+                    oper1 = result;
+                    flag = false;
+                }
+                operation = Constants.MULTIPLY;
+                break;
+
+            case R.id.btnLess:
+
+                if(oper1==0.0){
+                    oper1 = Double.valueOf(txtResult.getText().toString());
+                    flag = false;
+
+                }else if(oper2==0.0 && flag){
+                    oper2 = Double.valueOf(txtResult.getText().toString());
+
+                    double result = operation(operation, oper1, oper2);
+                    txtResult.setText(Double.toString(result));
+                    oper2 = 0.0;
+                    oper1 = result;
+                    flag = false;
+                }
+                operation = Constants.LESS;
+                break;
+
+            case R.id.btnDivision:
+
+                if(oper1==0.0){
+                    oper1 = Double.valueOf(txtResult.getText().toString());
+                    flag = false;
+
+                }else if(oper2==0.0 && flag){
+                    oper2 = Double.valueOf(txtResult.getText().toString());
+
+                    double result = operation(operation, oper1, oper2);
+                    txtResult.setText(Double.toString(result));
+                    oper2 = 0.0;
+                    oper1 = result;
+                    flag = false;
+                }
+                operation = Constants.DIVISION;
+                break;
+
+            case R.id.btnPlus:
+
+                if(oper1==0.0){
+                    oper1 = Double.valueOf(txtResult.getText().toString());
+                    flag = false;
+
+                }else if(oper2==0.0 && flag){
+                    oper2 = Double.valueOf(txtResult.getText().toString());
+
+                    double result = operation(operation, oper1, oper2);
+                    txtResult.setText(Double.toString(result));
+                    oper2 = 0.0;
+                    oper1 = result;
+                    flag = false;
+                }
+                operation = Constants.PLUS;
+                break;
+
+            default:
+                Button btnTemp = (Button) findViewById(v.getId());
+
+                if(flag) {
+                    txtResult.append(btnTemp.getText());
+                }else{
+                    txtResult.setText(btnTemp.getText());
+                    flag = true;
+                }
+                break;
+
+
+        }
+
+    }
+
+
+    public double operation(int typeOper,double oper1, double oper2){
+
+        double result = 0.0;
+
+        switch (typeOper){
+
+            case Constants.PLUS:
+                result = oper1 + oper2;
+
+                break;
+            case Constants.DIVISION:
+                result = oper1 / oper2;
+                break;
+            case Constants.LESS:
+                result = oper1 - oper2;
+                break;
+            case Constants.MULTIPLY:
+                result = oper1 * oper2;
+                break;
+
+        }
+        return result;
     }
 }
